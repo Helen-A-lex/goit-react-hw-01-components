79b128cf5b9855f239001c8f2a1782a43e959896
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-05-24 00:31:59 +0200

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import user from '../user.json';
+import data from "../data.json";
+import { Statistics } from './Statistics/Statistic';
+import { Profile } from './Profile/Profile';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+    <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
+      
+
+      <Statistics title="Upload stats" stats={data}
+      />
+   </div>
   );
 };
---
+++ src/components/Statistics/Statistic.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+          {title && <h2 className="title">{title}</h2>}
+
+  <ul className="stat-list">
+    <li className="item">
+    <span className="label">{stats.label}</span>
+    <span className="percentage">{stats.percentage}</span>
+    </li>
+    <li className="item">
+      <span className="label">{stats.label}</span>
+      <span className="percentage">{stats.percentage}</span>
+    </li>
+    <li className="item">
+      <span className="label">{stats.label}</span>
+      <span className="percentage">{stats.percentage}</span>
+    </li>
+    <li className="item">
+      <span className="label">{stats.label}</span>
+      <span className="percentage">{stats.percentage}</span>
+    </li>
+  </ul>
+</section>
+  );
+};
+Statistics.propTypes= {
+    title: PropTypes.string,
+    stats: PropTypes.shape({}),  
+}
